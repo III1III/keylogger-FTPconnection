f071a7555854a94bf39720234d4fe93ad52fc6c7
Gabriel <113034157+III1III@users.noreply.github.com>
2023-01-28 20:47:43 -0300

    Delete host.py

--- connection/host.py
+++
@@ -1,9 +0,0 @@
-import ftplib
-
-ftp = ftplib.FTP("ftp.example.com")
-ftp.login("username", "password")
-
-with open("log.txt", "wb") as f:
-    ftp.retrbinary("RETR log.txt", f.write)
-
-ftp.quit()
